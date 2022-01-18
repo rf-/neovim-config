14b7b9bf4e0ebd948c49d0cb43d5a9b46ab14ae6
Ryan Fitzgerald <rwfitzge@gmail.com>
2022-01-18 09:58:34 -0800

    Use sync formatting and null-ls code actions

--- format.fish
+++ format.fish
@@ -2,5 +2,5 @@
 
 for file in fnl/**/*.fnl
   echo $file
-  fnlfmt --fix --body-forms module --fn-forms defn,defn- $file
+  fnlfmt --fix --body-forms module,augroup! --fn-forms defn,defn- $file
 end
