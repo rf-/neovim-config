1f0f49127aefb7c1e7502851c39d77bbf9cecc55
Ryan Fitzgerald <rwfitzge@gmail.com>
2024-03-03 10:57:56 -0800

    Fix sync script for vendored lua libs

--- sync.fish
+++ sync.fish
@@ -3,7 +3,6 @@
 set -l config $HOME/.config/nvim
 
 mkdir -p $config/undo $config/spell $config/backup
-rm -rf $config/lua
 
 env MACOSX_DEPLOYMENT_TARGET=10.15 PACKER_SYNC=1 \
    nvim "+au User PackerComplete qa" "+PackerSync" "+silent TSUpdateSync"
