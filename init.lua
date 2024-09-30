cce7f902f2e6a5a307284143317524581a86a64b
Ryan Fitzgerald <rwfitzge@gmail.com>
2024-09-30 10:21:11 -0700

    Disable all config for VSCode

--- init.lua
+++ init.lua
@@ -7,119 +7,121 @@ local cmd = vim.api.nvim_command
 local fn = vim.fn
 local fmt = string.format
 
-local pack_path = fn.stdpath("data") .. "/site/pack"
+if not vim.g.vscode then
+  local pack_path = fn.stdpath("data") .. "/site/pack"
 
-function ensure(user, repo)
-  local install_path = fmt("%s/packer/start/%s", pack_path, repo)
-  if fn.empty(fn.glob(install_path)) > 0 then
-    cmd(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
-    cmd(fmt("packadd %s", repo))
+  function ensure(user, repo)
+    local install_path = fmt("%s/packer/start/%s", pack_path, repo)
+    if fn.empty(fn.glob(install_path)) > 0 then
+      cmd(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
+      cmd(fmt("packadd %s", repo))
+    end
   end
-end
 
-ensure("wbthomason", "packer.nvim")
+  ensure("wbthomason", "packer.nvim")
 
-require("packer").startup(function ()
-  -- Basic Packer/Fennel/Lua setup
+  require("packer").startup(function ()
+    -- Basic Packer/Fennel/Lua setup
 
-  use "wbthomason/packer.nvim"
-  use "Olical/aniseed"
-  use "Olical/conjure"
-  use "Olical/nvim-local-fennel"
-  use "tsbohc/zest.nvim"
+    use "wbthomason/packer.nvim"
+    use "Olical/aniseed"
+    use "Olical/conjure"
+    use "Olical/nvim-local-fennel"
+    use "tsbohc/zest.nvim"
 
-  use "nvim-lua/plenary.nvim"
+    use "nvim-lua/plenary.nvim"
 
-  -- Ergonomics
+    -- Ergonomics
 
-  use "godlygeek/tabular"
-  use "jeetsukumaran/vim-indentwise"
-  use "justinmk/vim-sneak"
-  use "tpope/vim-endwise"
-  use "tpope/vim-repeat"
-  use "tpope/vim-speeddating"
-  use "tpope/vim-surround"
-  use "tpope/vim-unimpaired"
+    use "godlygeek/tabular"
+    use "jeetsukumaran/vim-indentwise"
+    use "justinmk/vim-sneak"
+    use "tpope/vim-endwise"
+    use "tpope/vim-repeat"
+    use "tpope/vim-speeddating"
+    use "tpope/vim-surround"
+    use "tpope/vim-unimpaired"
 
-  use "kana/vim-textobj-user"
-  use "glts/vim-textobj-comment"
+    use "kana/vim-textobj-user"
+    use "glts/vim-textobj-comment"
 
-  use "preservim/nerdcommenter"
+    use "preservim/nerdcommenter"
 
-  use "tpope/vim-fugitive"
-  use "tpope/vim-rhubarb"
+    use "tpope/vim-fugitive"
+    use "tpope/vim-rhubarb"
 
-  use "airblade/vim-gitgutter"
+    use "airblade/vim-gitgutter"
 
-  use "rf-/vim-bclose"
+    use "rf-/vim-bclose"
 
-  use "AndrewRadev/splitjoin.vim"
+    use "AndrewRadev/splitjoin.vim"
 
-  use "simnalamburt/vim-mundo"
+    use "simnalamburt/vim-mundo"
 
-  -- Snippets
+    -- Snippets
 
-  use "hrsh7th/vim-vsnip"
-  use {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({})
-    end
-  }
-  use {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    config = function()
-      require("CopilotChat").setup({})
-    end
-  }
+    use "hrsh7th/vim-vsnip"
+    use {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      event = "InsertEnter",
+      config = function()
+        require("copilot").setup({})
+      end
+    }
+    use {
+      "CopilotC-Nvim/CopilotChat.nvim",
+      branch = "canary",
+      config = function()
+        require("CopilotChat").setup({})
+      end
+    }
 
-  -- Language support
+    -- Language support
 
-  vim.g["polyglot_disabled"] = {"typescript"}
-  use "sheerun/vim-polyglot"
-  use "rf-/yats.vim"
-  use "nvim-treesitter/nvim-treesitter"
+    vim.g["polyglot_disabled"] = {"typescript"}
+    use "sheerun/vim-polyglot"
+    use "rf-/yats.vim"
+    use "nvim-treesitter/nvim-treesitter"
 
-  use "neovim/nvim-lspconfig"
-  use "nvimtools/none-ls.nvim"
+    use "neovim/nvim-lspconfig"
+    use "nvimtools/none-ls.nvim"
 
-  use "folke/trouble.nvim"
-  use "seblj/nvim-echo-diagnostics"
+    use "folke/trouble.nvim"
+    use "seblj/nvim-echo-diagnostics"
 
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-cmdline"
-  use "hrsh7th/cmp-vsnip"
-  use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-buffer"
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-cmdline"
+    use "hrsh7th/cmp-vsnip"
+    use "hrsh7th/nvim-cmp"
 
-  use "vale1410/vim-minizinc"
-  use "nelstrom/vim-textobj-rubyblock"
+    use "vale1410/vim-minizinc"
+    use "nelstrom/vim-textobj-rubyblock"
 
-  use { "iamcco/markdown-preview.nvim", run = "cd app && yarn install" }
+    use { "iamcco/markdown-preview.nvim", run = "cd app && yarn install" }
 
-  -- Navigation
+    -- Navigation
 
-  use "nvim-telescope/telescope.nvim"
-  use "nvim-telescope/telescope-fzy-native.nvim"
-  use "nvim-telescope/telescope-ui-select.nvim"
+    use "nvim-telescope/telescope.nvim"
+    use "nvim-telescope/telescope-fzy-native.nvim"
+    use "nvim-telescope/telescope-ui-select.nvim"
 
-  use "preservim/nerdtree"
+    use "preservim/nerdtree"
 
-  -- Color
+    -- Color
 
-  use "rf-/edge"
-end)
+    use "rf-/edge"
+  end)
 
--- Fix Packer or whatever breaking load path
-package.path = vim.fs.normalize("~") .. "/.config/nvim/lua/?.lua," .. package.path
+  -- Fix Packer or whatever breaking load path
+  package.path = vim.fs.normalize("~") .. "/.config/nvim/lua/?.lua," .. package.path
 
-if not vim.env.PACKER_SYNC then
-  vim.g["aniseed#env"] = {
-    module = "local.init",
-    compile = true
-  }
+  if not vim.env.PACKER_SYNC then
+    vim.g["aniseed#env"] = {
+      module = "local.init",
+      compile = true
+    }
+  end
 end
