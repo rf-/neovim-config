956ce36905e1db9dfe6f7f937450706296e7f275
Ryan Fitzgerald <rwfitzge@gmail.com>
2024-12-03 16:08:39 -0800

    Disable code actions sign

--- fnl/local/plugins/lspsaga.fnl
+++ fnl/local/plugins/lspsaga.fnl
@@ -3,8 +3,6 @@
 (local {: keymap} vim)
 (local {:set map!} keymap)
 
-(lspsaga.setup {:symbol_in_winbar {:enable false}
-                :lightbulb {:virtual_text false}
-                :ui {:code_action "»"}})
+(lspsaga.setup {:symbol_in_winbar {:enable false} :lightbulb {:enable false}})
 
 (map! [:n :v] :<Leader>go ":Lspsaga outline<CR>")
--- lua/local/plugins/lspsaga.lua
+++ lua/local/plugins/lspsaga.lua
@@ -2,5 +2,5 @@
 local lspsaga = require("lspsaga")
 local keymap = vim["keymap"]
 local map_21 = keymap["set"]
-lspsaga.setup({symbol_in_winbar = {enable = false}, lightbulb = {virtual_text = false}, ui = {code_action = "\194\187"}})
+lspsaga.setup({symbol_in_winbar = {enable = false}, lightbulb = {enable = false}})
 return map_21({"n", "v"}, "<Leader>go", ":Lspsaga outline<CR>")
